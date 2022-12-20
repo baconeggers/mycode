130430afab4b1833fffae25ff7cd7c0d8900aad5
baconeggers <charlesmeggersii@gmail.com>
2022-12-20 18:39:33 +0000

    input function challenge

---
+++ lab_input/challenge.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Request user input for user_name
+user_name = input("Please enter your name: ")
+
+# Request user input for week_day
+week_day = input("What day of the week is it? ")
+
+# Returning the values to std out
+print("Hello,", user_name + "! Happy", week_day + "!")
